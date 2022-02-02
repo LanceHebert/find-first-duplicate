27f53a42496bc8370b404e071f2bf08bf43dfa3e
Lance Hebert <LSUHEBERT@gmail.com>
2022-02-02 14:47:38 -0800

    commit tree 392e4cc27aec5827122391b9a4de299ef1e68c94

--- javascript/find_first_duplicate.js
+++ javascript/find_first_duplicate.js
@@ -1,7 +1,19 @@
+// take first input, shift number out of array and do index of. If not -1 that is a duplicate. If -1 start over at first index and search again
+
 function findFirstDuplicate(arr) {
   // type your code here
-}
+  if (arr.length < 1){
+    return -1
+  }
+  for (let i = 0; i < arr.length; i++) {
+    let holder = arr[i];
 
+    if (arr.indexOf(holder, i + 1) > 0) {
+      return holder;
+    }
+    return -1
+  }
+}
 if (require.main === module) {
   // add your own tests in here
   console.log("Expecting: 3");
